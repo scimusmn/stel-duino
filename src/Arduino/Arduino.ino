0deb05ece944e5b3a2755a3f5ce8c60a3d294da1
Brandon <brandonwkipp@gmail.com>
2019-06-26 13:20:03 -0500

    refactor: Pulled latest submodule

--- src/Arduino/Arduino.ino
+++ src/Arduino/Arduino.ino
@@ -2,7 +2,7 @@
 #include "arduino-base/Libraries/Button.h"
 #include "arduino-base/Libraries/SerialManager.h"
 
-SerialManager manager;
+SerialManager serialManager;
 
 long baudRate = 115200;
 
@@ -10,17 +10,20 @@ AnalogInput analogInput1;
 Button button1;
 
 // Pin assignments
-int analogInputPin1 = A0;
-int buttonPin = 2;
-int ledPin = 3;
-int pwmOutputPin = 5;
+#define analogInput1Pin A0
+#define button1Pin 2
+#define ledPin 3
+#define pwmOutputPin 5
 
 void setup() {
 
+  // Enables/disables debug messaging from ArduinoJson
+  boolean arduinoJsonDebug = false;
+
   // Ensure Serial Port is open and ready to communicate
-  manager.setup(baudRate, [](String message, int value) {
+  serialManager.setup(baudRate, [](char* message, char* value) {
     onParse(message, value);
-  });
+  }, arduinoJsonDebug);
 
   // For every sketch, we need to set up our IO
   // Setup digital pins and default modes as needed, analog inputs are setup by default
@@ -32,12 +35,14 @@ void setup() {
   // Parameter 1: pin location
   // Parameter 2: enable averaging to get a less constant stream of data
   boolean enableAverager = true;
-  // Parameter 3: enable lowpass filter for Averager to further smooth value
-  boolean enableLowPass = true;
-  // Parameter 4: callback
+  // Parameter 3: the number of samples to average
+  int averagerSampleRate = 10;
+  // Parameter 4: enable lowpass filter for Averager to further smooth value
+  boolean enableLowPass = false;
+  // Parameter 5: callback
 
-  analogInput1.setup(analogInputPin1, enableAverager, enableLowPass, [](int analogInputValue) {
-    manager.sendJsonMessage("analog-input1", analogInputValue);
+  analogInput1.setup(analogInput1Pin, enableAverager, averagerSampleRate, enableLowPass, [](int analogInputValue) {
+    serialManager.sendJsonMessage("analogInput1", analogInputValue);
   });
 
   // DIGITAL INPUTS
@@ -45,35 +50,34 @@ void setup() {
   // Parameter 1: pin location
   // Parameter 2: callback
 
-  button1.setup(buttonPin, [](int state) {
-    if (state) manager.sendJsonMessage("button1-press", 1);
+  button1.setup(button1Pin, [](int state) {
+    if (state) serialManager.sendJsonMessage("button1-press", 1);
   });
 }
 
 void loop() {
   analogInput1.idle();
   button1.idle();
-  manager.idle();
+  serialManager.idle();
 }
 
-void onParse(String message, int value) {
-  if (message == "\"led\"" && value == 1) {
+void onParse(char* message, int value) {
+  if (strcmp(message, "led") == 0) {
     // Turn-on led
     digitalWrite(ledPin, value);
   }
-  else if (message == "\"led\"" && value == 0) {
-    // Turn-off led
-    digitalWrite(ledPin, value);
-  }
-  else if (message == "\"pwm-output\"" && value >= 0) {
+  else if (strcmp(message, "pwm-output") == 0 && value >= 0) {
     // Set pwm value to pwm pin
     analogWrite(pwmOutputPin, value);
-    manager.sendJsonMessage("pwm-set", value);
+    serialManager.sendJsonMessage("pwm-set", value);
   }
-  else if (message == "\"pot-rotation\"" && value == 1) {
-    manager.sendJsonMessage("pot-rotation", analogInput1.readValue());
+  else if (strcmp(message, "pot-rotation") == 0 && value == 1) {
+    serialManager.sendJsonMessage("pot-rotation", analogInput1.readValue());
+  }
+  else if (strcmp(message, "wake-arduino") == 0 && value == 1) {
+    serialManager.sendJsonMessage("arduino-ready", 1);
   }
   else {
-    manager.sendJsonMessage("unknown-command", 1);
+    serialManager.sendJsonMessage("unknown-command", 1);
   }
 }
